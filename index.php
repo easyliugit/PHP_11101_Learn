77f407420fa615dbeadd15c136e4260a86574380
alphaliu <easy19050711@gmail.com>
2022-04-22 15:31:09 +0800

    說明substr / mb_substr的使用方式

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <a href="./string/pra01.php">字串取代pra01.php</a><br>
 <a href="./string/pra02.php">字串分割pra02.php</a><br>
 <a href="./string/pra03.php">字串組合pra03.php</a><br>
+<a href="./string/pra04.php">子字串取用pra04.php</a><br>
 <?php 
 define('PI',3.1415);
 

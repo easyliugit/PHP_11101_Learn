6ada258077cd0892a671213f14a7b714f0ffce24
alphaliu <easy19050711@gmail.com>
2022-05-27 16:47:06 +0800

    搭配html列出所有資料並加上css做美化

--- db/php/index.php
+++ db/php/index.php
@@ -1,31 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP連線資料庫的方式</title>
     <style>
-        h1 ,h2 ,h3 ,h4{
+        h1,
+        h2,
+        h3,
+        h4 {
             text-align: center;
+        }
+
+        table {
+            border-collapse: collapse;
+            border: 3px solid blue;
+            margin: auto;
+        }
+
+        table td {
+            padding: 0.5rem;
+            border: 1px solid #aaa;
+        }
+
+        table tr:nth-child(odd) {
+            background: lightgreen;
+        }
+
+        table tr:nth-child(even) {
+            background: lightcyan;
+        }
+
+        table tr:hover {
+            background: lightcoral;
         }
     </style>
 </head>
+
 <body>
     <h1>PHP連線資料庫</h1>
     <?php
-        $dsn = "mysql:host=localhost;charset=utf8;dbname=school2";
-        $pdo = new PDO($dsn,'root','admin');
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=school2";
+    $pdo = new PDO($dsn, 'root', 'admin');
 
-        $sql = "SELECT * FROM `students`,`dept` WHERE `dept`.`id`= `students`.`dept`";
+    $sql="SELECT `students`.*,`dept`.`code`,`dept`.`name` as '科系' FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
+    
+    $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-        $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_BOTH); 
-
-        // print_r($rows);
-
-        echo $rows[0][3];
-        echo '<br>';
-        echo $rows[0]['birthday'];
+    echo "<table>";
+    foreach ($rows as $row) {
+        echo "<tr>";
+        echo "<td>{$row['id']}</td>";
+        echo "<td>{$row['school_num']}</td>";
+        echo "<td>{$row['name']}</td>";
+        echo "<td>{$row['科系']}</td>";
+        echo "<td>{$row['parents']}</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
     ?>
 </body>
+
 </html>

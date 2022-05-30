df6e5d28105027794da88d62bf45988e59547c30
alphaliu <easy19050711@gmail.com>
2022-05-30 10:54:44 +0800

    完成了新增資料寫入資料表

--- intermediate/db/php/add.php
+++ intermediate/db/php/add.php
@@ -17,27 +17,27 @@
     </style>
 </head>
 <body>
-    <form action="add.php" method="post">
+    <form action="store.php" method="post">
         <label for="">學號：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="uni_id" id=""><br>
         <label for="">班級座號：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="seat_num" id=""><br>
         <label for="">姓名：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="name" id=""><br>
         <label for="">生日：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="birthday" id=""><br>
         <label for="">身分證號碼：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="national_id" id=""><br>
         <label for="">住址：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="address" id=""><br>
         <label for="">家長：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="parent" id=""><br>
         <label for="">電話：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="telphone" id=""><br>
         <label for="">科別：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name="major" id=""><br>
         <label for="">畢業國中：</label>
-        <input type="text" name="" id=""><br>
+        <input type="text" name=secondary"" id=""><br>
         <button type="submit">新增</button>
         <button type="reset">重置</button>
     </form>

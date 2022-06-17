e12a2cb358f992d58ddfc871158b2723801c5fa8
alphaliu <easy19050711@gmail.com>
2022-06-17 14:16:36 +0800

    在新增投票功能上增加單複選題選項

--- intermediate/vote/back/add_vote.php
+++ intermediate/vote/back/add_vote.php
@@ -4,13 +4,18 @@
         <input type="text" name="subject" id="subject">
         <input type="button" value="新增選項" onclick="more()">
     </div>
+    <div id="selector">
+        <input type="radio" name="multiple" value="0" checked>
+        <label>單選</label>
+        <input type="radio" name="multiple" value="1" >
+        <label>複選</label>
+    </div>
     <div id="options">
         <div>
             <label>選項:</label><input type="text" name="option[]">
         </div>
     </div>
     <input type="submit" value="新增">
-
 </form>
 <script>
     function more(){

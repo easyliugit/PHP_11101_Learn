a61f069ec52118fde9986913a4a9dc3f5acb6637
alphaliu <easy19050711@gmail.com>
2022-06-17 11:48:25 +0800

    完成投票主題編輯功能

--- intermediate/vote/layout/front_nav.php
+++ intermediate/vote/layout/front_nav.php
@@ -1,4 +1,4 @@
-    <nav>
+<nav>
         <a href="index.php">回首頁</a>
         <a href="index.php">登入</a>
         <a href="index.php">投票列表</a>
--- intermediate/vote/layout/header.php
+++ intermediate/vote/layout/header.php
@@ -1,1 +1,2 @@
-<slider>輪播器</slider>
+
+    <div class="slider">輪播器</div>

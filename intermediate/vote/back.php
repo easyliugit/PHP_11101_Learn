c1f1a2cfe6ecc44b2f364804b2b41b01d32b034e
alphaliu <easy19050711@gmail.com>
2022-06-17 09:42:57 +0800

    增加原始碼註解

--- intermediate/vote/back.php
+++ intermediate/vote/back.php
@@ -1,3 +1,7 @@
+<?php
+
+include_once "./api/base.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>投票管理中心</title>
+    <!--使用拆分css檔案的方式來區分共用的css設定及前後台不同的css-->
     <link rel="stylesheet" href="./css/basic.css">
     <link rel="stylesheet" href="./css/back.css">
 </head>
@@ -16,16 +21,35 @@
 </div>
 <div id="container">
 <?php
+//根據網址有沒有帶do這個參數來決定要include那個外部檔案
 if(isset($_GET['do'])){
     $file="./back/".$_GET['do'].".php";
 }
+
+//判斷$file變數是否存在及$file所代表的檔案位置是否存在
 if(isset($file) && file_exists($file)){
     include $file;
 }else{
 ?>
     <button class="btn btn-primary" onclick="location.href='?do=add_vote'">新增投票</button>
     
-    <div>投票列表</div>
+    <div>
+        <ul>
+        <?php
+            //使用all()函式來取得資料表subjects中的所有資料，請參考base.php中的函式all($table,...$arg)
+            $subjects=all('subjects');
+
+            //使用迴圈將每一筆資料的內容顯示在畫面上
+            foreach($subjects as $subject){
+                echo "<li class='list-items'>";
+                echo $subject['subject'];
+                echo "</li>";
+            }
+
+        ?>
+        </ul>
+
+    </div>
 <?php
 }
 ?>

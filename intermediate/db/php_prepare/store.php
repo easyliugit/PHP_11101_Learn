36ec61141c7b0ca0975a59be63c47142a3c886e8
alphaliu <easy19050711@gmail.com>
2022-06-26 10:16:05 +0800

    PHP連線資料庫範例修改成預備語法，完成新增功能。

--- intermediate/db/php_prepare/store.php
+++ intermediate/db/php_prepare/store.php
@@ -1,11 +1,12 @@
 <?php
-$dsn="mysql:host=localhost;charset=utf8;dbname=school";
-$pdo=new PDO($dsn,'root','admin');
+//連線資料庫
+include("setup.php");
 
-$sql="INSERT INTO `students` (`uni_id`, `seat_num`, `name`, `birthday`, `national_id`, `address`, `parent`, `telphone`, `major`, `secondary`) 
-                       values( '{$_POST['uni_id']}', '{$_POST['seat_num']}', '{$_POST['name']}', '{$_POST['birthday']}', '{$_POST['national_id']}', '{$_POST['address']}', '{$_POST['parent']}', '{$_POST['telphone']}', '{$_POST['major']}', '{$_POST['secondary']}')";
-$pdo->query($sql);
-
-//echo $sql;
+$sql_query = "INSERT INTO students (uni_id ,seat_num ,name ,birthday ,national_id ,address ,parent ,telphone ,major ,secondary) VALUES (?, ?, ?, ? ,? ,? ,? ,? ,? ,?)";
+$stmt = $db_link -> prepare($sql_query);
+$stmt -> execute(array($_POST["uni_id"], $_POST["seat_num"], $_POST["name"], $_POST["birthday"], $_POST["national_id"], $_POST["address"], $_POST["parent"], $_POST["telphone"], $_POST["major"], $_POST["secondary"]));
+$stmt = null;
+$db_link = null;
+//重新導向回到主畫面
 header("location:index.php");
 ?>

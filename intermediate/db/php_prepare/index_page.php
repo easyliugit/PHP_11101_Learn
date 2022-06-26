1122ac1b75b3038e6b53ba1f16c5febe7e1bb77e
alphaliu <easy19050711@gmail.com>
2022-06-26 12:25:49 +0800

    PHP連線資料庫範例修改成預備語法，追加分頁功能。

--- intermediate/db/php_prepare/index_page.php
+++ intermediate/db/php_prepare/index_page.php
@@ -1,10 +1,31 @@
 <?php
 //連線資料庫
 include("setup.php");
-$sql_query="SELECT * FROM `students` WHERE 1";
-$stmt = $db_link->prepare($sql_query);
+
+//預設每頁筆數
+$pageRow_records = 3;
+//預設頁數
+$num_pages = 1;
+//若已經有翻頁，將頁數更新
+if (isset($_GET['page'])) {
+  $num_pages = $_GET['page'];
+}
+//本頁開始記錄筆數 = (頁數-1)*每頁記錄筆數
+$startRow_records = ($num_pages -1) * $pageRow_records;
+
+$sql_query="SELECT * FROM students";
+//加上限制顯示筆數的SQL敘述句，由本頁開始記錄筆數開始，每頁顯示預設筆數
+$sql_query_limit = $sql_query." LIMIT {$startRow_records}, {$pageRow_records}";
+//以加上限制顯示筆數的SQL敘述句查詢資料到 $stmt 中
+$stmt = $db_link->prepare($sql_query_limit);
 $stmt->execute();
-$total_records = $stmt->rowCount();
+//以未加上限制顯示筆數的SQL敘述句查詢資料到 $all_stmt 中
+$all_stmt = $db_link->prepare($sql_query);
+$all_stmt->execute();
+//計算總筆數
+$total_records = $all_stmt->rowCount();
+//計算總頁數=(總筆數/每頁筆數)後無條件進位。
+$total_pages = ceil($total_records/$pageRow_records);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -55,7 +76,7 @@ $total_records = $stmt->rowCount();
     <h3><button><a href="add.php">新增學生資料</a></button></h3>
     <h3><form action="add.php" method="get"><button>新增學生資料</button></form></h3>
     <h3><button onclick="location.href='add.php'">新增學生資料</button></h3>
-    <p>目前資料筆數：<?php echo $total_records;?> | <a href="index_page.php">分頁版</a></p>
+    <p>目前資料筆數：<?php echo $total_records;?> | <a href="index.php">不分頁</a></p>
     <table>
     <tr>
         <td>序號</td>
@@ -82,25 +103,31 @@ $total_records = $stmt->rowCount();
             echo "</td>";
             echo "</tr>";
         }
-        // $row_result=$stmt->fetchAll();
-        // echo "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>";
-	    // foreach($row_result as $item=>$row){
-        //     echo "<tr>";
-        //     echo "<td>{$row['id']}</td>";
-        //     echo "<td>{$row['uni_id']}</td>";
-        //     echo "<td>{$row['name']}</td>";
-        //     echo "<td>{$row['major']}</td>";
-        //     echo "<td>{$row['parent']}</td>";
-        //     echo "<td>{$row['secondary']}</td>";
-        //     echo "<td>";
-        //     echo "<button><a href='edit.php?id={$row['id']}'>編輯</a></button>";
-        //     echo "<button><a href='del.php?id={$row['id']}'>刪除</a></button>";
-        //     echo "</td>";
-        //     echo "</tr>";
-        // }
     }
     ?>
     </table>
+    <p>
+        <?php if ($num_pages > 1) { // 若不是第一頁則顯示 ?>
+            <a href="index_page.php?page=1">第一頁</a> | 
+            <a href="index_page.php?page=<?php echo $num_pages-1;?>">上一頁</a>
+        <?php } ?>
+        <?php if ($num_pages < $total_pages) { // 若不是最後一頁則顯示 ?>
+            <a href="index_page.php?page=<?php echo $num_pages+1;?>">下一頁</a> | 
+            <a href="index_page.php?page=<?php echo $total_pages;?>">最後頁</a>
+        <?php } ?>
+    </p>
+    <p>
+    頁數：
+  	  <?php
+  	  for($i=1;$i<=$total_pages;$i++){
+  	  	  if($i==$num_pages){
+  	  	  	  echo $i." ";
+  	  	  }else{
+  	  	      echo "<a href=\"index_page.php?page={$i}\">{$i}</a> ";
+  	  	  }
+  	  }
+  	  ?>
+    </p>
 </body>
 
 </html>

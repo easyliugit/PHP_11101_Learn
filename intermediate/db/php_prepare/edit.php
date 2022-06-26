031eb36ed2ed4991bb3e3e57e75b69b462272b98
alphaliu <easy19050711@gmail.com>
2022-06-26 10:34:26 +0800

    PHP連線資料庫範例修改成預備語法，完成編輯頁面顯示。

--- intermediate/db/php_prepare/edit.php
+++ intermediate/db/php_prepare/edit.php
@@ -1,3 +1,12 @@
+
+<?php
+//連線資料庫
+include("setup.php");
+$sql_query="SELECT * FROM `students` WHERE id = ?";
+$stmt = $db_link->prepare($sql_query);
+$stmt->execute(array($_GET['id']));
+$row=$stmt->fetch();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,26 +28,17 @@
 </head>
 <body>
     <h1>編輯學生資料</h1>
-    <?php
-
-        $id=$_GET['id'];
-        $dsn="mysql:host=localhost;charset=utf8;dbname=school";
-        $pdo=new PDO($dsn,'root','admin');
-        $sql="SELECT * FROM `students` WHERE `id`='$id'";
-        $student=$pdo->query($sql)->fetch();
-
-    ?>
     <form action="update.php" method="post">
-    <label for="">學號：<input type="text" name="uni_id" value="<?=$student['uni_id'];?>"></label>
-        <label for="">班級座號：<input type="text" name="seat_num" value="<?=$student['seat_num'];?>"></label>
-        <label for="">姓名：<input type="text" name="name" value="<?=$student['name'];?>"></label>
-        <label for="">生日<input type="text" name="birthday" value="<?=$student['birthday'];?>"></label>
-        <label for="">身分證號碼<input type="text" name="national_id" value="<?=$student['national_id'];?>"></label>
-        <label for="">住址<input type="text" name="address" value="<?=$student['address'];?>"></label>
-        <label for="">家長<input type="text" name="parent" value="<?=$student['parent'];?>"></label>
-        <label for="">電話<input type="text" name="telphone" value="<?=$student['telphone'];?>"></label>
-        <label for="">科別<input type="text" name="major" value="<?=$student['major'];?>"></label>
-        <label for="">畢業國中<input type="text" name="secondary" value="<?=$student['secondary'];?>"></label>
+    <label for="">學號：<input type="text" name="uni_id" value="<?=$row['uni_id'];?>"></label>
+        <label for="">班級座號：<input type="text" name="seat_num" value="<?=$row['seat_num'];?>"></label>
+        <label for="">姓名：<input type="text" name="name" value="<?=$row['name'];?>"></label>
+        <label for="">生日<input type="text" name="birthday" value="<?=$row['birthday'];?>"></label>
+        <label for="">身分證號碼<input type="text" name="national_id" value="<?=$row['national_id'];?>"></label>
+        <label for="">住址<input type="text" name="address" value="<?=$row['address'];?>"></label>
+        <label for="">家長<input type="text" name="parent" value="<?=$row['parent'];?>"></label>
+        <label for="">電話<input type="text" name="telphone" value="<?=$row['telphone'];?>"></label>
+        <label for="">科別<input type="text" name="major" value="<?=$row['major'];?>"></label>
+        <label for="">畢業國中<input type="text" name="secondary" value="<?=$row['secondary'];?>"></label>
         <input type="hidden" name="id" value="<?=$id;?>">
         <input type="submit" value="更新">
         <input type="reset" value="重置">

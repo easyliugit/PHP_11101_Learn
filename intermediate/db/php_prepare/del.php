4b8424201d28fb3107e760608f406721e4e2d063
alphaliu <easy19050711@gmail.com>
2022-06-26 11:33:26 +0800

    PHP連線資料庫範例修改成預備語法，完成刪除功能。

--- intermediate/db/php_prepare/del.php
+++ intermediate/db/php_prepare/del.php
@@ -1,13 +1,22 @@
 <?php
+//連線資料庫
+include("setup.php");
+$sql_query = "DELETE FROM students WHERE id=?";
+$stmt = $db_link -> prepare($sql_query);
+$stmt -> execute(array($_GET['id']));
+$stmt = null;
+$db_link = null;
 
-$id=$_GET['id'];
-$dsn="mysql:host=localhost;charset=utf8;dbname=school";
-$pdo=new PDO($dsn,'root','admin');
 
-$sql="DELETE FROM `students` WHERE `id`='$id'";
+// $id=$_GET['id'];
+// $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+// $pdo=new PDO($dsn,'root','admin');
 
-$pdo->exec($sql);
+// $sql="DELETE FROM `students` WHERE `id`='$id'";
 
+// $pdo->exec($sql);
+
+//重新導向回到主畫面
 header("location:index.php");
 
 ?>

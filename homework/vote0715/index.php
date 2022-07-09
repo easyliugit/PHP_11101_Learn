9b66d966c7b161822a51b3da2d5b2b9efb6c5c51
alphaliu <easy19050711@gmail.com>
2022-07-10 00:56:53 +0800

    投票系統使用者資料，防止帳號重複與未數入密碼。

--- homework/vote0715/index.php
+++ homework/vote0715/index.php
@@ -5,20 +5,32 @@ $action=$_REQUEST['action'];
 // 顯示頁面與資料庫存取
 switch($action){
     case "users_add":
-    users_add($db_link);
-    header("location:{$_SERVER['PHP_SELF']}");
+        //找尋帳號是否已經註冊
+        $query_RecFindUser = "SELECT count(*) FROM votedb_users WHERE u_user='{$_POST["u_user"]}'";
+        $RecFindUser=$db_link->query($query_RecFindUser);
+        if($RecFindUser->fetchColumn()){            
+            $Msg = "此帳號已被使用";
+            header("location:{$_SERVER['PHP_SELF']}?action=users_add_form&u_user={$_POST["u_user"]}&Msg={$Msg}");
+        }elseif($_POST["u_pw"]==""){
+            $Msg = "請輸入密碼";
+            header("location:{$_SERVER['PHP_SELF']}?action=users_add_form&u_user={$_POST["u_user"]}&Msg={$Msg}");
+
+        }else{
+            // die_content("測試= 此帳號可以使用");
+            users_add($db_link);
+            header("location:{$_SERVER['PHP_SELF']}");
+        }
     case "users_add_form":
-    $content=voteWeb(users_add_form());
+        $content=voteWeb(users_add_form());
     break; 
     default:
-    $content=voteWeb(users_list());
+        $content=voteWeb(users_list());
 }
 echo $content;
 
 function users_add($db_link){
     global $link;
     $sql_query = "INSERT INTO votedb_users (u_user ,u_pw ,u_nick ,u_email ,u_jointime) VALUES (?, ?, ?, ?, NOW())";
-    // die_content("測試= {$sql_query}");
     $stmt = $db_link -> prepare($sql_query);
     $stmt -> execute(array(
         FilterString($_POST["u_user"], 'string')
@@ -31,25 +43,26 @@ function users_add($db_link){
 }
 function users_add_form(){
     global $link;
+    $MsgFinUser = $MsgFinUser;
     $main='
     <form action="'.$_SERVER['PHP_SELF'].'" method="post">
     <fieldset>
-        <legend>新增使用者資料</legend>
-        <dl>
-            <dt>使用帳號</dt>
-            <dd><input type="text" name="u_user" id="u_user"></dd>
-            <dt>使用密碼</dt>
-            <dd><input type="password" name="u_pw" id="u_pw"></dd>
-            <dt>暱稱</dt>
-            <dd><input type="text" name="u_nick" id="u_nick"></dd>
-            <dt>電子郵件</dt>
-            <dd><input type="email" name="u_email" id="u_email"></dd>
-        </dl>
+    <legend>新增使用者資料</legend>
+    <dl>
+    <dt>使用帳號</dt>
+    <dd><input type="text" name="u_user" id="u_user" value="'.$_GET["u_user"].'">'.$_GET["Msg"].'</dd>
+    <dt>使用密碼</dt>
+    <dd><input type="password" name="u_pw" id="u_pw" value=""></dd>
+    <dt>暱稱</dt>
+    <dd><input type="text" name="u_nick" id="u_nick" value=""></dd>
+    <dt>電子郵件</dt>
+    <dd><input type="email" name="u_email" id="u_email" value=""></dd>
+    </dl>
     </fieldset>
     <input type="hidden" name="action" value="users_add">
     <input type="submit" value="送出">
     <input type="reset" value="重置">
-</form>
+    </form>
     ';
     return $main;
 }

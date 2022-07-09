1bbe680a5e040ceae14309551c589e0d2e9c44a6
alphaliu <easy19050711@gmail.com>
2022-07-09 14:14:42 +0800

    PHP連線資料庫範例修改成預備語法，首頁修改查詢筆數3種用法。

--- intermediate/db/php_prepare/index.php
+++ intermediate/db/php_prepare/index.php
@@ -3,8 +3,26 @@
 include("setup.php");
 $sql_query="SELECT * FROM `students` WHERE 1";
 $stmt = $db_link->prepare($sql_query);
+
+// 計算總筆數 方法1.
 $stmt->execute();
-$total_records = $stmt->rowCount();
+$total_records = count($stmt->fetchAll());
+// 方法1.結束
+
+$stmt->execute();
+
+// 計算總筆數 方法2. 3. 適用僅查詢筆數
+// 計算總筆數 方法2.
+$sql_query2="SELECT count(*) FROM `students` WHERE 1";
+$stmt2 = $db_link->prepare($sql_query2);
+$stmt2->execute();
+$total_records2 = ($stmt2->fetch())[0];
+// 方法2.結束
+
+// 計算總筆數 方法3.(效能最好) 沿用方法2.sql
+$stmt2->execute();
+$total_records3 = $stmt2->fetchColumn();
+// 方法3.結束
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -55,7 +73,8 @@ $total_records = $stmt->rowCount();
     <h3><button><a href="add.php">新增學生資料</a></button></h3>
     <h3><form action="add.php" method="get"><button>新增學生資料</button></form></h3>
     <h3><button onclick="location.href='add.php'">新增學生資料</button></h3>
-    <p>目前資料筆數：<?php echo $total_records;?> | <a href="index_page.php">分頁版</a></p>
+    <p><a href="index_page.php">分頁版</a></p>
+    <p>目前資料筆數：方法1=<?php echo $total_records;?> | 方法2=<?php echo $total_records2;?> | 方法3=<?php echo $total_records3;?></a></p>
     <table>
     <tr>
         <td>序號</td>
@@ -68,10 +87,6 @@ $total_records = $stmt->rowCount();
     </tr>
     <?php
     if($total_records){
-        // 指標執行過一次while($row=$stmt->fetch())結束
-        // 重複輸出必須重新$stmt->execute()
-        // execute()內可用array()調整query的欄位值
-        // 適用query 可變動的欄位值
         while($row=$stmt->fetch()){
             echo "<tr>";
             echo "<td>{$row['id']}</td>";
@@ -86,7 +101,6 @@ $total_records = $stmt->rowCount();
             echo "</td>";
             echo "</tr>";
         }
-        // 相同query，請用fetchAll，可以重複使用foreach輸出
         // $row_result=$stmt->fetchAll();
         // echo "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>";
 	    // foreach($row_result as $item=>$row){

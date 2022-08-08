62acaba32e50ba069dbae702922c6e7053ff2037
alphaliu <easy19050711@gmail.com>
2022-08-08 11:19:48 +0800

    完成預告片排序功能

---
+++ advanced/bquiz03/back/add_move.php
@@ -0,0 +1,26 @@
+<h3>新增院線片</h3>
+<form action="./api/add_move.php" method="post">
+    <table style="width: 75%;margin:auto;">
+    <tr>
+        <td>影片資料</td>
+        <td>
+            <div><label for="">片名</label><input type="text" name="" id=""></div>
+            <div><label for="">分級</label><input type="text" name="" id=""></div>
+            <div><label for="">片長</label><input type="text" name="" id=""></div>
+            <div><label for="">上映日期</label><input type="text" name="" id=""></div>
+            <div><label for="">發行商</label><input type="text" name="" id=""></div>
+            <div><label for="">導演</label><input type="text" name="" id=""></div>
+            <div><label for=""></label><input type="text" name="" id=""></div>
+            <div><label for=""></label><input type="text" name="" id=""></div>
+            <div><label for=""></label><input type="text" name="" id=""></div>
+            <div><label for=""></label><input type="text" name="" id=""></div>
+            </td>
+    </tr>
+    <tr>
+        <td>劇情簡介</td>
+        <td>
+            <textarea name="intro" style="width: 90%;height: 60px;" id="" cols="30" rows="10"></textarea>
+        </td>
+    </tr>
+</table>
+</form>

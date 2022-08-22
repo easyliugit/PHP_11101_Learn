7a56058ebddf30915e2b73fe43a0fe8053f617cb
alphaliu <easy19050711@gmail.com>
2022-08-22 10:30:20 +0800

    建立主要功能項目的include頁面檔案

--- advanced/bquiz04/back.php
+++ advanced/bquiz04/back.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!-- saved from url=(0057)?do=admin -->
+<!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
@@ -16,29 +16,42 @@
     <iframe name="back" style="display:none;"></iframe>
     <div id="main">
         <div id="top">
-            <a href="index.php">
+            <a href="?">
                 <img src="./icon/0416.jpg">
             </a>
-            <img src="./icon/0417.jpg">
+            <div style="padding:10px;">
+                <a href="?">回首頁</a> |
+                <a href="?do=news">最新消息</a> |
+                <a href="?do=look">購物流程</a> |
+                <a href="?do=buycart">購物車</a> |
+                <a href="?do=login">會員登入</a> |
+                <a href="?do=admin">管理登入</a>
+            </div> 
+            情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~
         </div>
         <div id="left" class="ct">
             <div style="min-height:400px;">
-                <a href="?do=admin&redo=admin">管理權限設置</a>
-                <a href="?do=admin&redo=th">商品分類與管理</a>
-                <a href="?do=admin&redo=order">訂單管理</a>
-                <a href="?do=admin&redo=mem">會員管理</a>
-                <a href="?do=admin&redo=bot">頁尾版權管理</a>
-                <a href="?do=admin&redo=news">最新消息管理</a>
-                <a href="?do=admin&redo=logout" style="color:#f00;">登出</a>
+            選單區
             </div>
+            <span>
+                <div>進站總人數</div>
+                <div style="color:#f00; font-size:28px;">
+                    00005 </div>
+            </span>
         </div>
         <div id="right">
-		<?php
-		include "./back/admin.php";
+        <?php 
+        $do=$_GET['do']??'main';
+        $file="./front/".$do.".php";
+        if(file_exists($file)){
+                include $file;
+        }else{
+                include "./front/main.php";
+        }
+        ?>
 
-		?>
         </div>
-        <div id="bottom" style="line-height:70px; color:#FFF; background:url(icon/bot.png);" class="ct">
+        <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
             頁尾版權 : </div>
     </div>
 

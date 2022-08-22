bcdf8040da9ce03820dbac9dd02c2ec0fb8f8100
alphaliu <easy19050711@gmail.com>
2022-08-22 12:06:52 +0800

    建立base.php檔案,完成後台頁尾版權編輯功能

--- advanced/bquiz04/back.php
+++ advanced/bquiz04/back.php
@@ -1,6 +1,7 @@
+<?php include_once "base.php";?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!-- saved from url=(0039) -->
+<!-- saved from url=(0057)?do=admin -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
@@ -16,43 +17,36 @@
     <iframe name="back" style="display:none;"></iframe>
     <div id="main">
         <div id="top">
-            <a href="?">
+            <a href="index.php">
                 <img src="./icon/0416.jpg">
             </a>
-            <div style="padding:10px;">
-                <a href="?">回首頁</a> |
-                <a href="?do=news">最新消息</a> |
-                <a href="?do=look">購物流程</a> |
-                <a href="?do=buycart">購物車</a> |
-                <a href="?do=login">會員登入</a> |
-                <a href="?do=admin">管理登入</a>
-            </div> 
-            情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~
+            <img src="./icon/0417.jpg">
         </div>
         <div id="left" class="ct">
             <div style="min-height:400px;">
-            選單區
+                <a href="?do=admin">管理權限設置</a>
+                <a href="?do=th">商品分類與管理</a>
+                <a href="?do=order">訂單管理</a>
+                <a href="?do=mem"> 會員管理</a>
+                <a href="?do=bot">頁尾版權管理</a>
+                <a href="?do=news">最新消息管理</a>
+                <a href="?do=logout" style="color:#f00;">登出</a>
             </div>
-            <span>
-                <div>進站總人數</div>
-                <div style="color:#f00; font-size:28px;">
-                    00005 </div>
-            </span>
         </div>
         <div id="right">
         <?php 
-        $do=$_GET['do']??'main';
-        $file="./front/".$do.".php";
+        $do=$_GET['do']??'admin';
+        $file="./back/".$do.".php";
         if(file_exists($file)){
                 include $file;
         }else{
-                include "./front/main.php";
+                include "./front/admin.php";
         }
         ?>
 
         </div>
-        <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
-            頁尾版權 : </div>
+        <div id="bottom" style="line-height:70px; color:#FFF; background:url(icon/bot.png);" class="ct">
+           <?=$Bot->find(1)['bot'];?> </div>
     </div>
 
 </body>

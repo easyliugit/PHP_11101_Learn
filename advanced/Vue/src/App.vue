936e5ab6552fe132bf68e20d4ad82effa71f08b3
alphaliu <easy19050711@gmail.com>
2022-09-07 07:55:43 +0800

    使用emit 來傳送刪除todo的指令

--- advanced/Vue/src/App.vue
+++ advanced/Vue/src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
-  import {reactive} from 'vue'
+  import {ref,reactive} from 'vue'
+  import ListItem from './components/ListItem.vue';
   const form=reactive({todo:'',due:'',type:1})
   const todos=reactive([]);
   const addTodo=()=>{
     todos.push({todo:form.todo,due:form.due,type:form.type})
-    console.log(todos)
     form.todo=''
     form.due=''
     form.type=1
-    console.log(form)
   }
   const delTodo=(idx)=>{
     todos.splice(idx,1)
+    console.log('刪除',idx)
   }
   </script>
   
@@ -26,37 +26,17 @@
       <option value="2">普通</option>
       <option value="3">不重要</option>
     </select><br>
+    
     <button @click="addTodo">新增</button>
   </div>
   <div>
-    <div class="list-item" v-for="todo,idx in todos" :key="idx">
-      <div>{{ todo.todo }}</div>
-      <div>{{ todo.due }}</div>
-      <div>{{ todo.type }}</div>
-      <button @click="delTodo(idx)">X</button>
-    </div>
+   <!-- v-for="todo,idx in todos" :key="idx" ref="items" -->
+   <ListItem v-for="todo,idx in todos" :key="idx" :todo="todo" @delTodo="delTodo(idx)"/>
   </div>
   </template>
   
   <style scoped>
   .header{
     text-align: center;
-  }
-  .list-item{
-    border:1px solid #999;
-    display:flex;
-    box-shadow: 0 0 3px #ccc;
-    margin:0.5rem 2rem;
-    padding:0.5rem;
-    font-size:1.5rem
-  }
-  .list-item > div:nth-child(1){
-    width:12rem;
-  }
-  .list-item > div:nth-child(2){
-    width:10rem;
-  }
-  .list-item > div:nth-child(3){
-    width:5rem;
   }
   </style>
---
+++ advanced/Vue/src/components/ListItem.vue
@@ -0,0 +1,57 @@
+<script setup>
+    import {ref,reactive} from 'vue'
+    defineProps({todo:Object})
+    const emits=defineEmits(['delTodo'])
+    const item=ref();
+    const delTodo=()=>{
+        emits('delTodo')
+    }
+    const editTodo=()=>{
+      //console.log(items.value[idx].childNodes)
+      item.value.childNodes[0].innerHTML=`<input type='text' v-model=\"form.todo\" value="${item.value.childNodes[0].innerText}">`
+      item.value.childNodes[1].innerHTML=`<input type='date' v-model=\"form.due\" value="${item.value.childNodes[1].innerText}">`
+      item.value.childNodes[2].innerHTML=`
+      <select name="type"  v-model="form.type">
+        <option value="1">很重要</option>
+        <option value="2">普通</option>
+        <option value="3">不重要</option>
+      </select>
+      `
+      let button=document.createElement('button')
+      let node=document.createTextNode('OK');
+      button.appendChild(node)
+      item.value.childNodes[3].remove()
+      item.value.childNodes[3].remove()
+      item.value.appendChild(button)
+    }
+    </script>
+    <template>
+    
+    <div class="list-item" ref="item">
+        <div>{{ todo.todo }}</div>
+        <div>{{ todo.due }}</div>
+        <div>{{ todo.type }}</div>
+        <button @click="editTodo">編輯</button>
+        <button @click="delTodo">X</button>
+      </div>
+    
+    </template>
+    <style scoped>
+    .list-item{
+      border:1px solid #999;
+      display:flex;
+      box-shadow: 0 0 3px #ccc;
+      margin:0.5rem 2rem;
+      padding:0.5rem;
+      font-size:1.5rem
+    }
+    .list-item > div:nth-child(1){
+      width:12rem;
+    }
+    .list-item > div:nth-child(2){
+      width:10rem;
+    }
+    .list-item > div:nth-child(3){
+      width:5rem;
+    }
+    </style>
